1254b7c88403ceaa305ce24f1ab793c5faea1af7
Harsh <harshgautam697@gmail.com>
2025-07-11 20:39:54 +0530

    Updated music

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Background Music sunai de rha h?',
+        title: 'Background Music Sunna Hai?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
